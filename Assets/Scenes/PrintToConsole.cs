ee73b9c76686b39ad12157a411e5b401d8626c64
daniel <danielsoledad@gmail.com>
2021-09-02 14:09:51 -0700

    Adding first Log to console via C#

--- Assets/Scenes/PrintToConsole.cs
+++ Assets/Scenes/PrintToConsole.cs
@@ -7,7 +7,7 @@ public class PrintToConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("This game is the best of the worst games so far stay tone!");
     }
 
     // Update is called once per frame
